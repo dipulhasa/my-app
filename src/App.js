9b39f5182ff57e161f26c7b54f2ae55b8f10e99e
dipul-hasan <dipul@technobd.com>
2021-11-18 14:37:21 +0600

    app line number 6 heading rename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 function App(){
 return <div>
-          <h1 className="HeadingStyle" >Hello World</h1>
+          <h1 className="HeadingStyle" >Hello Dipul, How Are You?</h1>
           <Card textTitle="We are Dipul Hasan Group" textDesc="Lorem ipsum Dolur Set Amit duminy Text" />
           <Card textTitle="We are Dipul Asif Group" textDesc="Lorem ipsum Dolur Set Amit duminy Text"/>
           <Card textTitle="We are Dipul Rubel Group" textDesc="Lorem ipsum Dolur Set Amit duminy Text"/>
